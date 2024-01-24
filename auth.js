013ae510d3101f0546d9b24b144eb24f6780a8d7
Terct <servercentralterct@gmail.com>
2024-01-24 16:47:19 -0300

    first commit

---
+++ auth.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+mongoose.connect('mongodb://admin:Mfcd62!!Mfcd62!!@85.239.239.3:23743/mydatabase?authSource=admin&readPreference=primary&ssl=false&directConnection=true', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erro de conexão ao MongoDB:'));
+db.once('open', () => {
+  console.log('Conectado ao MongoDB sem especificar o banco de dados');
+  // Especificar o banco de dados
+  const dbName = 'SAAS_Evolution';
+  const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    pass: String,
+    status_plan: String,
+  });
+
+  const User = mongoose.model('User', UserSchema, 'users');
+
+  // Registro de usuário
+  app.post('/register', async (req, res) => {
+    const { name, email, pass, status_plan } = req.body;
+
+    // Hash da senha antes de salvar no banco de dados
+    const hashedPassword = await bcrypt.hash(pass, 10);
+
+    const newUser = new User({
+      name,
+      email,
+      pass: hashedPassword,
+      status_plan,
+    });
+
+    try {
+      await newUser.save();
+      res.status(201).send('Usuário registrado com sucesso');
+    } catch (error) {
+      res.status(500).send('Erro ao registrar o usuário');
+    }
+  });
+
+  // Autenticação de usuário (login)
+  app.post('/login', async (req, res) => {
+    const { email, pass } = req.body;
+
+    try {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(404).send('Usuário não encontrado');
+      }
+
+      const isPasswordValid = await bcrypt.compare(pass, user.pass);
+
+      if (!isPasswordValid) {
+        return res.status(401).send('Senha inválida');
+      }
+
+      const token = jwt.sign({ email: user.email }, 'your-secret-key', {
+        expiresIn: '1h',
+      });
+
+      res.status(200).json({ token });
+    } catch (error) {
+      res.status(500).send('Erro durante o login');
+    }
+  });
+
+  app.listen(port, () => {
+    console.log(`Servidor está rodando em http://localhost:${port}`);
+  });
+});
