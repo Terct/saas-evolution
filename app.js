b66dc258a7aff64dd31be62aeffe995b8eb45668
Terct <servercentralterct@gmail.com>
2024-01-28 07:48:16 -0300

    first commit

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const dotenv = require('dotenv'); // Importe a biblioteca dotenv
-// Carregue as variáveis de ambiente do arquivo .env
+const dotenv = require('dotenv');
+
+const fs = require('fs/promises');
+const path = require('path');
+const axios = require('axios');
+
 dotenv.config();
 
 const app = express();
@@ -116,21 +120,201 @@ db.once('open', () => {
   });
 
 
-
-
   app.post('/webhooks/messages-upsert', async (req, res) => {
-    
-  
     try {
-    
-      console.log(req.body)
+      let message;
+
+      if (req.body.data.message.extendedTextMessage && req.body.data.message.extendedTextMessage.text) {
+        message = req.body.data.message.extendedTextMessage.text;
+      } else if (req.body.data.message.conversation) {
+        message = req.body.data.message.conversation;
+      }
+
+      if (!message) {
+        console.error('Mensagem não encontrada');
+        res.status(400).send('Mensagem não encontrada');
+        return;
+      }
+
+      let remoteid = req.body.data.key.remoteJid;
+      let server_url = req.body.server_url;
+      let apikey = req.body.apikey;
+      let instancia = req.body.data.owner;
+
+      // Procurar o usuário com base nos valores de instancia e apikey
+      const user = await User.findOne({ instanciaName: instancia, instanciaApikey: apikey });
+
+      if (!user) {
+        return res.status(404).send('Usuário não encontrado para a instância e apikey fornecidas');
+      }
+
+      let chatRetune = user.retuneChat;
+      let apikeyRetune = user.retuneApikey;
+      let threadIdChat;
+
+      let threadIdFile = `./src/retune/threadId/${instancia}/data.json`;
+
+      try {
+        const threadIdData = await fs.readFile(threadIdFile, 'utf8');
+        const threadIdJson = JSON.parse(threadIdData);
+
+        if (threadIdJson[remoteid]) {
+          threadIdChat = threadIdJson[remoteid];
+          console.log(`ThreadId encontrado para remoteid ${remoteid}: ${threadIdChat}`);
+
+        } else {
+          const retuneApiUrl = `https://retune.so/api/chat/${chatRetune}/new-thread`;
+          const retuneHeaders = {
+            'Content-Type': 'application/json',
+            'X-Workspace-API-Key': apikeyRetune,
+          };
+
+          const retuneResponse = await axios.post(retuneApiUrl, {}, { headers: retuneHeaders });
+          threadIdChat = retuneResponse.data.threadId;
+
+          threadIdJson[remoteid] = threadIdChat;
+          await fs.writeFile(threadIdFile, JSON.stringify(threadIdJson), 'utf8');
+
+          console.log(`Novo threadId gerado para remoteid ${remoteid}: ${threadIdChat}`);
+        }
+      } catch (readError) {
+        const retuneApiUrl = `https://retune.so/api/chat/${chatRetune}/new-thread`;
+        const retuneHeaders = {
+          'Content-Type': 'application/json',
+          'X-Workspace-API-Key': apikeyRetune,
+        };
+
+        const retuneResponse = await axios.post(retuneApiUrl, {}, { headers: retuneHeaders });
+        threadIdChat = retuneResponse.data.threadId;
+
+        await fs.mkdir(path.dirname(threadIdFile), { recursive: true });
+        await fs.writeFile(threadIdFile, JSON.stringify({ [remoteid]: threadIdChat }), 'utf8');
+
+        console.log(`Novo threadId gerado para remoteid ${remoteid}: ${threadIdChat}`);
+      }
+
+      try {
+        const retuneApiUrl = `https://retune.so/api/chat/${chatRetune}/response`;
+        const retuneHeaders = {
+          'Content-Type': 'application/json',
+          'X-Workspace-API-Key': apikeyRetune,
+        };
+
+        const retuneRequestBody = {
+          threadId: threadIdChat,
+          input: message,
+        };
+
+        const retuneResponse = await axios.post(retuneApiUrl, retuneRequestBody, { headers: retuneHeaders });
+
+        // Aqui você pode lidar com a resposta da segunda requisição
+        console.log('Resposta da requisição para retune.so:', retuneResponse.data);
+
+
+
+
+
+        try {
+          const sendMessageUrl = `${server_url}/message/sendText/${instancia}`;
+          const sendMessageHeaders = {
+            'Content-Type': 'application/json',
+            'apikey': apikey,
+          };
+
+          const sendMessageBody = {
+            number: remoteid,
+            options: {
+              delay: 1200,
+              presence: 'composing',
+              linkPreview: false,
+            },
+            textMessage: {
+              text: retuneResponse.data.response.value,
+            },
+          };
+
+          const sendMessageResponse = await axios.post(sendMessageUrl, sendMessageBody, { headers: sendMessageHeaders });
+
+          // Aqui você pode lidar com a resposta da terceira requisição
+          console.log('Resposta da requisição para enviar mensagem:', sendMessageResponse.data);
+
+          // Resto do seu código aqui...
+
+        } catch (sendMessageError) {
+          console.error('Erro durante a requisição para enviar mensagem:', sendMessageError);
+          res.status(500).send('Erro durante a requisição para enviar mensagem');
+        }
+
+
+
+
+      } catch (retuneError) {
+        console.error('Erro durante a requisição para retune.so:', retuneError);
+        res.status(500).send('Erro durante a requisição para retune.so');
+      }
+
+
 
     } catch (error) {
       console.error('Erro durante a adição dos valores:', error);
       res.status(500).send('Erro durante a adição dos valores');
     }
   });
-  
+
+
+
+
+
+
+  app.post('/historic', async (req, res) => {
+    const { instancia, apikey } = req.body;
+
+    try {
+
+      // Fazendo a requisição usando axios
+      const response = await axios.post(`https://evolution.dagestao.com/chat/findMessages/${instancia}`, {
+        // Se houver dados adicionais a serem enviados no corpo da solicitação, adicione aqui
+      }, {
+        headers: {
+          'apikey': apikey,
+          'Content-Type': 'application/json',  // Defina o tipo de conteúdo conforme necessário
+        },
+      });
+
+      // Se a solicitação for bem-sucedida, você pode acessar os dados de resposta
+      const chat = response.data;
+
+      //console.log(responseData)
+
+      // Objeto para armazenar as mensagens agrupadas por remoteJid
+      const chatsPorRemoteJid = {};
+
+      // Itera sobre cada mensagem no chat
+      chat.forEach((mensagem) => {
+        // Verifica se a mensagem tem owner "Dev01"
+        if (mensagem.owner === "Dev01") {
+          // Obtém o remoteJid da mensagem
+          const remoteJid = mensagem.key.remoteJid;
+
+          // Cria um array para o remoteJid se ainda não existir
+          chatsPorRemoteJid[remoteJid] = chatsPorRemoteJid[remoteJid] || [];
+
+          // Adiciona a mensagem ao array correspondente ao remoteJid
+          chatsPorRemoteJid[remoteJid].push(mensagem);
+          //console.log(mensagem)
+        }
+      });
+
+      console.log(chatsPorRemoteJid);
+
+
+      res.status(200).json({ chatsPorRemoteJid });
+
+    } catch (error) {
+      res.status(500).send('Erro ao buscar o historico');
+    }
+  });
+
 
 
   app.listen(port, () => {
---
+++ exemple-convert.js
@@ -0,0 +1,459 @@
+const chat = [
+    {
+        "key": {
+            "id": "EA43D7A564A61F84D2D29C872CA52DE3",
+            "remoteJid": "553488612071@s.whatsapp.net",
+            "fromMe": true
+        },
+        "_id": "65b3df733f7d3c6fe9bd012a",
+        "pushName": "Armando",
+        "messageType": "conversation",
+        "message": {
+            "conversation": "Tese 3"
+        },
+        "source": "android",
+        "messageTimestamp": 1706286963,
+        "owner": "Dev01",
+        "__v": 0
+    },
+    {
+        "key": {
+            "id": "4C84BBF0A4A7EEBD27040E1972953DCE",
+            "remoteJid": "553488612071@s.whatsapp.net",
+            "fromMe": true
+        },
+        "_id": "65b3df703f7d3c6fe9bd011a",
+        "pushName": "Armando",
+        "messageType": "conversation",
+        "message": {
+            "conversation": "Teste 2"
+        },
+        "source": "android",
+        "messageTimestamp": 1706286960,
+        "owner": "Dev01",
+        "__v": 0
+    },
+    {
+        "key": {
+            "id": "964C2EC14739BF1A055C1DE19490779B",
+            "remoteJid": "553488612071@s.whatsapp.net",
+            "fromMe": true
+        },
+        "_id": "65b3df6d3f7d3c6fe9bd010a",
+        "pushName": "Armando",
+        "messageType": "conversation",
+        "message": {
+            "conversation": "Teste 1"
+        },
+        "source": "android",
+        "messageTimestamp": 1706286957,
+        "owner": "Dev01",
+        "__v": 0
+    },
+    {
+        "key": {
+            "id": "3AC237F43BD8DAF07660",
+            "remoteJid": "553488612071@s.whatsapp.net",
+            "fromMe": false
+        },
+        "_id": "65b3df673f7d3c6fe9bd00ff",
+        "pushName": "Divino",
+        "messageType": "conversation",
+        "message": {
+            "conversation": "Não",
+            "messageContextInfo": {
+                "deviceListMetadata": {
+                    "senderKeyIndexes": [],
+                    "recipientKeyIndexes": [],
+                    "senderKeyHash": "Dn4yODPpS57dlQ==",
+                    "senderTimestamp": {
+                        "low": 1705534491,
+                        "high": 0,
+                        "unsigned": true
+                    },
+                    "recipientKeyHash": "cbQWG6efglvKeg==",
+                    "recipientTimestamp": {
+                        "low": 1706284576,
+                        "high": 0,
+                        "unsigned": true
+                    }
+                },
+                "deviceListMetadataVersion": 2,
+                "messageSecret": "",
+                "paddingBytes": "",
+                "messageAddOnDurationInSecs": 0,
+                "botMessageSecret": "",
+                "botMetadata": null
+            }
+        },
+        "source": "ios",
+        "messageTimestamp": 1706286951,
+        "owner": "Dev01",
+        "__v": 0
+    },
+    {
+        "key": {
+            "id": "588F1BF84BE8DCE31EAA3220004A940E",
+            "remoteJid": "553488612071@s.whatsapp.net",
+            "fromMe": true
+        },
+        "_id": "65b3df603f7d3c6fe9bd00ed",
+        "pushName": "Armando",
+        "messageType": "conversation",
+        "message": {
+            "conversation": "Blz???"
+        },
+        "source": "android",
+        "messageTimestamp": 1706286944,
+        "owner": "Dev01",
+        "__v": 0
+    },
+    {
+        "key": {
+            "id": "3A2AAF9306B2083A9DCB",
+            "remoteJid": "553488612071@s.whatsapp.net",
+            "fromMe": false
+        },
+        "_id": "65b3df583f7d3c6fe9bd00e2",
+        "pushName": "Divino",
+        "messageType": "conversation",
+        "message": {
+            "conversation": "Oi",
+            "messageContextInfo": {
+                "deviceListMetadata": {
+                    "senderKeyIndexes": [],
+                    "recipientKeyIndexes": [],
+                    "senderKeyHash": "Dn4yODPpS57dlQ==",
+                    "senderTimestamp": {
+                        "low": 1705534491,
+                        "high": 0,
+                        "unsigned": true
+                    },
+                    "recipientKeyHash": "cbQWG6efglvKeg==",
+                    "recipientTimestamp": {
+                        "low": 1706284576,
+                        "high": 0,
+                        "unsigned": true
+                    }
+                },
+                "deviceListMetadataVersion": 2,
+                "messageSecret": "",
+                "paddingBytes": "",
+                "messageAddOnDurationInSecs": 0,
+                "botMessageSecret": "",
+                "botMetadata": null
+            }
+        },
+        "source": "ios",
+        "messageTimestamp": 1706286936,
+        "owner": "Dev01",
+        "__v": 0
+    },
+    {
+        "key": {
+            "id": "0AFEC98A83E577F102BB33FB839DC85D",
+            "remoteJid": "553488612071@s.whatsapp.net",
+            "fromMe": true
+        },
+        "_id": "65b3df523f7d3c6fe9bd00d0",
+        "pushName": "Armando",
+        "messageType": "conversation",
+        "message": {
+            "conversation": "Oie"
+        },
+        "source": "android",
+        "messageTimestamp": 1706286930,
+        "owner": "Dev01",
+        "__v": 0
+    },
+    {
+        "key": {
+            "id": "3A2903B10E09644C9E11",
+            "remoteJid": "553488612071@s.whatsapp.net",
+            "fromMe": false
+        },
+        "_id": "65b3df4d3f7d3c6fe9bd00c5",
+        "pushName": "Divino",
+        "messageType": "conversation",
+        "message": {
+            "conversation": "Como posso usar essa receita",
+            "messageContextInfo": {
+                "deviceListMetadata": {
+                    "senderKeyIndexes": [],
+                    "recipientKeyIndexes": [],
+                    "senderKeyHash": "Dn4yODPpS57dlQ==",
+                    "senderTimestamp": {
+                        "low": 1705534491,
+                        "high": 0,
+                        "unsigned": true
+                    },
+                    "recipientKeyHash": "cbQWG6efglvKeg==",
+                    "recipientTimestamp": {
+                        "low": 1706284576,
+                        "high": 0,
+                        "unsigned": true
+                    }
+                },
+                "deviceListMetadataVersion": 2,
+                "messageSecret": "",
+                "paddingBytes": "",
+                "messageAddOnDurationInSecs": 0,
+                "botMessageSecret": "",
+                "botMetadata": null
+            }
+        },
+        "source": "ios",
+        "messageTimestamp": 1706286925,
+        "owner": "Dev01",
+        "__v": 0
+    },
+    {
+        "key": {
+            "id": "C174EBAA5398162891",
+            "remoteJid": "553498092640@s.whatsapp.net",
+            "fromMe": false
+        },
+        "_id": "65b3db0f3f7d3c6fe9bd00b0",
+        "pushName": "D & A Gestão",
+        "messageType": "extendedTextMessage",
+        "message": {
+            "extendedTextMessage": {
+                "text": "Como posso ajudar você hoje?",
+                "matchedText": "",
+                "canonicalUrl": "",
+                "description": "",
+                "title": "",
+                "textArgb": 0,
+                "backgroundArgb": 0,
+                "font": 0,
+                "previewType": 0,
+                "jpegThumbnail": "",
+                "doNotPlayInline": false,
+                "thumbnailDirectPath": "",
+                "thumbnailSha256": "",
+                "thumbnailEncSha256": "",
+                "mediaKey": "",
+                "mediaKeyTimestamp": {
+                    "low": 0,
+                    "high": 0,
+                    "unsigned": false
+                },
+                "thumbnailHeight": 0,
+                "thumbnailWidth": 0,
+                "inviteLinkGroupType": 0,
+                "inviteLinkParentGroupSubjectV2": "",
+                "inviteLinkParentGroupThumbnailV2": "",
+                "inviteLinkGroupTypeV2": 0,
+                "viewOnce": false
+            },
+            "messageContextInfo": {
+                "deviceListMetadata": {
+                    "senderKeyIndexes": [],
+                    "recipientKeyIndexes": [],
+                    "senderKeyHash": "zIOpn7/maF+YEA==",
+                    "senderTimestamp": {
+                        "low": 1706216352,
+                        "high": 0,
+                        "unsigned": true
+                    },
+                    "recipientKeyHash": "cbQWG6efglvKeg==",
+                    "recipientTimestamp": {
+                        "low": 0,
+                        "high": 0,
+                        "unsigned": true
+                    }
+                },
+                "deviceListMetadataVersion": 2,
+                "messageSecret": "",
+                "paddingBytes": "",
+                "messageAddOnDurationInSecs": 0,
+                "botMessageSecret": "",
+                "botMetadata": null
+            }
+        },
+        "source": "web",
+        "messageTimestamp": 1706285839,
+        "owner": "Dev01",
+        "__v": 0
+    },
+    {
+        "key": {
+            "id": "63021FE817D7FE2539A40FD495490A33",
+            "remoteJid": "559991813125@s.whatsapp.net",
+            "fromMe": true
+        },
+        "_id": "65b3d9ef3f7d3c6fe9bd008b",
+        "pushName": "Armando",
+        "messageType": "extendedTextMessage",
+        "message": {
+            "extendedTextMessage": {
+                "text": "Tudo bem?",
+                "previewType": 0,
+                "inviteLinkGroupTypeV2": 0,
+                "matchedText": "",
+                "canonicalUrl": "",
+                "description": "",
+                "title": "",
+                "textArgb": 0,
+                "backgroundArgb": 0,
+                "font": 0,
+                "jpegThumbnail": "",
+                "doNotPlayInline": false,
+                "thumbnailDirectPath": "",
+                "thumbnailSha256": "",
+                "thumbnailEncSha256": "",
+                "mediaKey": "",
+                "mediaKeyTimestamp": {
+                    "low": 0,
+                    "high": 0,
+                    "unsigned": false
+                },
+                "thumbnailHeight": 0,
+                "thumbnailWidth": 0,
+                "inviteLinkGroupType": 0,
+                "inviteLinkParentGroupSubjectV2": "",
+                "inviteLinkParentGroupThumbnailV2": "",
+                "viewOnce": false
+            }
+        },
+        "source": "android",
+        "messageTimestamp": 1706285550,
+        "owner": "Dev01",
+        "__v": 0
+    },
+    {
+        "key": {
+            "id": "364F630FA62985FF4DCEA262B10BEDD5",
+            "remoteJid": "559991813125@s.whatsapp.net",
+            "fromMe": true
+        },
+        "_id": "65b3d9d83f7d3c6fe9bd007f",
+        "pushName": "Armando",
+        "messageType": "extendedTextMessage",
+        "message": {
+            "extendedTextMessage": {
+                "text": "Oie",
+                "previewType": 0,
+                "inviteLinkGroupTypeV2": 0,
+                "matchedText": "",
+                "canonicalUrl": "",
+                "description": "",
+                "title": "",
+                "textArgb": 0,
+                "backgroundArgb": 0,
+                "font": 0,
+                "jpegThumbnail": "",
+                "doNotPlayInline": false,
+                "thumbnailDirectPath": "",
+                "thumbnailSha256": "",
+                "thumbnailEncSha256": "",
+                "mediaKey": "",
+                "mediaKeyTimestamp": {
+                    "low": 0,
+                    "high": 0,
+                    "unsigned": false
+                },
+                "thumbnailHeight": 0,
+                "thumbnailWidth": 0,
+                "inviteLinkGroupType": 0,
+                "inviteLinkParentGroupSubjectV2": "",
+                "inviteLinkParentGroupThumbnailV2": "",
+                "viewOnce": false
+            }
+        },
+        "source": "android",
+        "messageTimestamp": 1706285528,
+        "owner": "Dev01",
+        "__v": 0
+    }
+]
+
+
+
+// Objeto para armazenar as mensagens agrupadas por remoteJid
+const chatsPorRemoteJid = {};
+
+// Itera sobre cada mensagem no chat
+chat.forEach((mensagem) => {
+  // Verifica se a mensagem tem owner "Dev01"
+  if (mensagem.owner === "Dev01") {
+    // Obtém o remoteJid da mensagem
+    const remoteJid = mensagem.key.remoteJid;
+
+    // Cria um array para o remoteJid se ainda não existir
+    chatsPorRemoteJid[remoteJid] = chatsPorRemoteJid[remoteJid] || [];
+
+    // Adiciona a mensagem ao array correspondente ao remoteJid
+    chatsPorRemoteJid[remoteJid].push(mensagem);
+    //console.log(mensagem)
+  }
+});
+
+historicData = chatsPorRemoteJid
+
+// Agora, chatsPorRemoteJid contém objetos separados para cada remoteJid,
+// cada um contendo um array de mensagens com owner "Dev01"
+//console.log(chatsPorRemoteJid);
+
+function preencherListaComRemoteIds(historicData) {
+    const lista = [];
+  
+    // Iterar sobre as chaves (remoteIds)
+    for (const remoteId in historicData) {
+      if (historicData.hasOwnProperty(remoteId)) {
+        // Adicionar o objeto à lista
+        lista.push({
+          id: lista.length + 1, // ou ajuste conforme necessário
+          name: remoteId,
+          img: "https://assets.appsmith.com/widgets/default.png"
+        });
+      }
+    }
+  
+    return lista;
+  }
+  
+  // Chamar a função e obter a lista preenchida
+  //const listaPreenchida = preencherListaComRemoteIds(historicData);
+  
+  // Agora, a variável 'listaPreenchida' contém os objetos formatados conforme desejado
+  //console.log(listaPreenchida);
+
+
+  function createChatHtmlForRemoteId(historicData, selectedRemoteId) {
+    let chatHtml = '';
+  
+    const selectedRemoteIdData = historicData[selectedRemoteId];
+  
+    if (!selectedRemoteIdData) {
+      return 'RemoteId não encontrado';
+    }
+    
+  
+    selectedRemoteIdData.forEach(item => {
+      const messageClass = item.pushName === 'Armando' ? 'right' : 'left';
+      const messageText = getMessageText(item.messageType, item.message);
+      const messageHtml = `<div class="message ${messageClass}">${messageText}</div>`;
+      chatHtml += messageHtml;
+    });
+  
+    const chatContainerHtml = `<div class="chat-container">${chatHtml}</div>`;
+    return chatContainerHtml;
+  }
+  
+  function getMessageText(messageType, message) {
+    switch (messageType) {
+      case 'extendedTextMessage':
+        return message.extendedTextMessage.text;
+      case 'conversation':
+        return message.conversation;
+      // Adicione mais casos conforme necessário para outros tipos de mensagem
+      default:
+        return 'Tipo de mensagem não reconhecido';
+    }
+  }
+  
+  // Exemplo de uso para o remoteId '553488612071@s.whatsapp.net':
+  const selectedRemoteId = '553488612071@s.whatsapp.net';
+  const chatHtml = createChatHtmlForRemoteId(historicData, selectedRemoteId);
+  console.log(chatHtml);
